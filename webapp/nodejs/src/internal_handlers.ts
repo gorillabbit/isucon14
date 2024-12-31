8ed556de47b25a2915c98b60180603d162149e03
gorillabbit <modsyoukaizenryoku@gmail.com>
2024-12-31 20:45:52 +0900

    速度と距離を考慮して椅子をマッチングする

--- webapp/nodejs/src/internal_handlers.ts
+++ webapp/nodejs/src/internal_handlers.ts
@@ -2,47 +2,84 @@ import type { Context } from "hono";
 import type { Environment } from "./types/hono.js";
 import type { RowDataPacket } from "mysql2";
 import type { Chair, Ride } from "./types/models.js";
+import { calculateDistance } from "./common.js";
 
 // このAPIをインスタンス内から一定間隔で叩かせることで、椅子とライドをマッチングさせる
 export const internalGetMatching = async (ctx: Context<Environment>) => {
-  // MEMO: 一旦最も待たせているリクエストに適当な空いている椅子マッチさせる実装とする。おそらくもっといい方法があるはず…
   const [rides] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
-    "SELECT * FROM rides WHERE chair_id IS NULL"
+    "SELECT * FROM rides WHERE chair_id IS NULL",
   );
-  console.log(rides);
   if (rides.length < 1) {
     return ctx.body(null, 204);
   }
+  const [matched] = await ctx.var.dbConn.query<Array<Chair & RowDataPacket>>(
+    `
+WITH latest_locations AS (
+    SELECT chair_id, latitude, longitude
+    FROM chair_locations
+    WHERE (chair_id, created_at) IN (
+        SELECT chair_id, MAX(created_at)
+        FROM chair_locations
+        GROUP BY chair_id
+    )
+),
+incomplete_rides AS (
+    SELECT DISTINCT r.chair_id
+    FROM rides r
+    JOIN ride_statuses rs ON r.id = rs.ride_id
+    GROUP BY r.chair_id, r.id
+    HAVING COUNT(rs.chair_sent_at) < 6
+)
+SELECT c.id, cm.speed, ll.latitude, ll.longitude
+FROM chairs c
+INNER JOIN chair_models cm ON c.model = cm.name
+LEFT JOIN latest_locations ll ON ll.chair_id = c.id
+WHERE c.is_active = TRUE
+AND NOT EXISTS (
+    SELECT 1
+    FROM incomplete_rides ir
+    WHERE ir.chair_id = c.id
+)
+      `,
+  );
+  if (matched.length < 1) {
+    return ctx.body(null, 204);
+  }
+  let i = 0;
+  const used_chairs: String[] = [];
   for (const ride of rides) {
-    let matched!: Chair & RowDataPacket;
-    let empty = false;
-    for (let i = 0; i < 10; i++) {
-      [[matched]] = await ctx.var.dbConn.query<Array<Chair & RowDataPacket>>(
-        "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"
-      );
-      console.log(matched);
-      if (!matched) {
-        return ctx.body(null, 204);
+    let fastest_chiar = { id: "", time: Infinity };
+    for (const match of matched) {
+      if (used_chairs.includes(match.id)) {
+        continue;
       }
-      const [[result]] = await ctx.var.dbConn.query<
-        Array<{ "COUNT(*) = 0": number } & RowDataPacket>
-      >(
-        "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE",
-        [matched.id]
+      const distant_1 = calculateDistance(
+        match.latitude,
+        match.longitude,
+        ride.pickup_latitude,
+        ride.pickup_longitude,
       );
-      empty = !!result["COUNT(*) = 0"];
-      if (empty) {
-        break;
+      const distant_2 = calculateDistance(
+        ride.destination_latitude,
+        ride.destination_longitude,
+        ride.pickup_latitude,
+        ride.pickup_longitude,
+      );
+      const time = (distant_1 + distant_2) / match.speed;
+      if (fastest_chiar.time > time) {
+        (fastest_chiar.id = match.id), (fastest_chiar.time = time);
       }
     }
-    if (!empty) {
-      return ctx.body(null, 204);
+    if (fastest_chiar.id) {
+      await ctx.var.dbConn.query("UPDATE rides SET chair_id = ? WHERE id = ?", [
+        fastest_chiar.id,
+        ride.id,
+      ]);
+      used_chairs.push(fastest_chiar.id);
+      i++;
+    } else {
+      break;
     }
-    console.log(ride.id);
-    await ctx.var.dbConn.query("UPDATE rides SET chair_id = ? WHERE id = ?", [
-      matched.id,
-      ride.id,
-    ]);
   }
   return ctx.body(null, 204);
 };
