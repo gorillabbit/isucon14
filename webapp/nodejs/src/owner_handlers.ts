b7a28429f94715e8fc32516c2332657f150a940f
gorillabbit <modsyoukaizenryoku@gmail.com>
2025-01-03 22:06:02 +0900

    f

--- webapp/nodejs/src/owner_handlers.ts
+++ webapp/nodejs/src/owner_handlers.ts
@@ -52,7 +52,14 @@ export const ownerGetSales = async (ctx: Context<Environment>) => {
     const modelSalesByModel: { [key: string]: number } = {};
     for (const chair of chairs) {
       const [rides] = await ctx.var.dbConn.query<Array<Ride & RowDataPacket>>(
-        "SELECT rides.* FROM rides JOIN ride_statuses ON rides.id = ride_statuses.ride_id WHERE chair_id = ? AND status = 'COMPLETED' AND updated_at BETWEEN ? AND ? + INTERVAL 999 MICROSECOND",
+        `
+        SELECT rides.* 
+        FROM rides 
+        JOIN ride_statuses ON rides.id = ride_statuses.ride_id 
+        WHERE chair_id = ? 
+        AND status = 'COMPLETED' 
+        AND updated_at BETWEEN ? AND ? + INTERVAL 999 MICROSECOND
+        `,
         [chair.id, since, until],
       );
       const sales = sumSales(rides);
@@ -106,27 +113,34 @@ export const ownerGetChairs = async (ctx: Context<Environment>) => {
   const [chairs] = await ctx.var.dbConn.query<
     Array<ChairWithDetail & RowDataPacket>
   >(
-    `SELECT id,
-       owner_id,
-       name,
-       access_token,
-       model,
-       is_active,
-       created_at,
-       updated_at,
-       IFNULL(total_distance, 0) AS total_distance,
-       total_distance_updated_at
-FROM chairs
-       LEFT JOIN (SELECT chair_id,
-                          SUM(IFNULL(distance, 0)) AS total_distance,
-                          MAX(created_at)          AS total_distance_updated_at
-                   FROM (SELECT chair_id,
-                                created_at,
-                                ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-                                ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
-                         FROM chair_locations) tmp
-                   GROUP BY chair_id) distance_table ON distance_table.chair_id = chairs.id
-WHERE owner_id = ?`,
+    `
+    SELECT id,
+      owner_id,
+      name,
+      access_token,
+      model,
+      is_active,
+      created_at,
+      updated_at,
+      IFNULL(total_distance, 0) AS total_distance,
+      total_distance_updated_at
+    FROM chairs
+      LEFT JOIN (
+        SELECT 
+          chair_id,
+          SUM(IFNULL(distance, 0)) AS total_distance,
+          MAX(created_at)          AS total_distance_updated_at
+        FROM (
+          SELECT 
+            chair_id,
+            created_at,
+            ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
+            ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+          FROM chair_locations
+        ) tmp
+        GROUP BY chair_id
+      ) distance_table ON distance_table.chair_id = chairs.id
+    WHERE owner_id = ?`,
     [owner.id],
   );
 
