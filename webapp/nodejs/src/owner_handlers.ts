9792b706b7f0bfe6a448b13403e2c52c040678ea
gorillabbit <modsyoukaizenryoku@gmail.com>
2025-01-05 22:41:07 +0900

    f

--- webapp/nodejs/src/owner_handlers.ts
+++ webapp/nodejs/src/owner_handlers.ts
@@ -122,20 +122,17 @@ export const ownerGetChairs = async (ctx: Context<Environment>) => {
       is_active,
       created_at,
       updated_at,
-      IFNULL(total_distance2, 0) AS total_distance,
+      total_distance,
       total_distance_updated_at
     FROM chairs
       LEFT JOIN (
         SELECT 
           chair_id,
-          SUM(IFNULL(distance, 0)) AS total_distance2,
           MAX(created_at)          AS total_distance_updated_at
         FROM (
           SELECT 
             chair_id,
-            created_at,
-            ABS(latitude - LAG(latitude) OVER (PARTITION BY chair_id ORDER BY created_at)) +
-            ABS(longitude - LAG(longitude) OVER (PARTITION BY chair_id ORDER BY created_at)) AS distance
+            created_at
           FROM chair_locations
         ) tmp
         GROUP BY chair_id
